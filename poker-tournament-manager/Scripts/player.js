242602d44ef456a36cde696884d820d4a2e03a1b
rodhall85 <rodhall@hotmail.co.uk>
2016-07-11 23:59:02 +0100

    First commit

---
+++ poker-tournament-manager/Scripts/player.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    function Player(id, name, sex, dob, style, description) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.dob = dob;
+        this.style = style;
+        this.description = description;
+    }
+});
