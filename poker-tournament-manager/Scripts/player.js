7d9482c69df500210fe69ccc2d8403fbe35b50d2
rodhall85 <rodhall@hotmail.co.uk>
2016-07-15 00:03:11 +0100

    Create Player page with basic functionality

--- poker-tournament-manager/Scripts/player.js
+++ poker-tournament-manager/Scripts/player.js
@@ -1,5 +1,11 @@
-﻿$(function () {
-    function Player(id, name, sex, dob, style, description) {
+﻿(function () {
+    var app = angular.module('player' , [ ]);
+
+    app.controller('PlayerController', function () {
+        this.player = new Player(1, 'Rod Hall', 'Male', '15/07/1985', 'Loose', 'Description');
+    });
+    
+    function Player(id, name4, sex, dob, style, description) {
         this.id = id;
         this.name = name;
         this.sex = sex;
@@ -7,4 +13,5 @@
         this.style = style;
         this.description = description;
     }
+
 });
