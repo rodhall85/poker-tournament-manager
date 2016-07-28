97e7d90146502181e2c9553d9d448d56046422dc
rodhall85 <rodhall@hotmail.co.uk>
2016-07-28 23:48:21 +0100

    Update player display and refactor angular into directives

--- poker-tournament-manager/Scripts/_references.js
+++ poker-tournament-manager/Scripts/_references.js
@@ -4,4 +4,4 @@
 /// <reference path="angular-route.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="jquery-1.9.1.js" />
-/// <reference path="player.js" />
+/// <reference path="player/player.js" />
