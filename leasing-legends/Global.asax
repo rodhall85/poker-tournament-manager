59249ab24f3570b1959b16133eccfd68e8559727
rodhall85 <rodhall@hotmail.co.uk>
2016-08-25 22:11:10 +0100

    Reverting bad commit

--- leasing-legends/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="leasing_legends.MvcApplication" Language="C#" %>
---
+++ server/app.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let express = require('express');
+let mongoUtil = require('./mongoUtil');
+let bodyParser = require('body-parser');
+let app = express();
+
+mongoUtil.connect();
+
+app.use(express.static(__dirname + "/../client") );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/players', (request, response) => {
+  let players = mongoUtil.players();
+  players.find().toArray((err,docs) => {
+    if(err) {
+      response.sendStatus(400);
+    }
+    console.log(JSON.stringify(docs));
+    response.json(docs);
+  });
+});
+
+app.post('/players/add', (request, response) => {
+  let player = request.body.player;
+  if (player == undefined) {
+    response.sendStatus(400);
+  }
+  else {
+    let players = mongoUtil.players();
+    console.log(JSON.stringify(player));
+    players.insert(player);
+    response.sendStatus(201);
+  }
+});
+
+app.listen(8181, () => console.log("Listening on port 8181"));
