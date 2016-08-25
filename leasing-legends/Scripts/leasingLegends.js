0f88408949ecc4b5c392bb65ee76469d19fc370e
Rod Hall <rod.hall@comparethemarket.com>
2016-08-25 13:20:06 +0100

    Add angular

---
+++ leasing-legends/Scripts/leasingLegends.js
@@ -0,0 +1,19 @@
+﻿angular
+  .module('leasingLegends', [])
+  .controller('pricesController',['$http', '$scope', pricesController]);
+
+function pricesController($http, $scope) {
+
+    $scope.test = "hello";
+
+    $http.get('../Data/data.json').then(
+        function(prices) {
+            $scope.prices = prices.data;
+            
+        },
+        function(err) {
+            console.log(err);
+        });
+    };
+
+
